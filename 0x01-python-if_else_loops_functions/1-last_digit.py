0157ff57adef975844c5b5990679177531d61031
sabah-hue <sabah.abdelbaset@gmail.com>
2023-11-29 02:11:29 +0200

    My First commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
-if last_digit < 0:
-    last_digit = -1 * last_digit
+if number < 0:
+    last_digit = number % (-10)
 if last_digit > 5:
     message = "greater than 5"
 elif last_digit == 0:
