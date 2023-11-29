86ee750770be3f86d0d0c196a31f85530549c26c
sabah-hue <sabah.abdelbaset@gmail.com>
2023-11-29 03:16:46 +0200

    My First commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for first_number in range(0, 10, 1):
     for last_number in range(first_number + 1, 10, 1):
         if first_number == 8 and last_number == 9:
-             print('{}{}'.format(first_number, last_number))
+            print('{}{}'.format(first_number, last_number))
         else:
-             print('{}{}, '.format(first_number, last_number), end="")
-
+            print('{}{}, '.format(first_number, last_number), end="")
