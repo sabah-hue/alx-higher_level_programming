277253c2c55abc1c33c07ca2a1e0dd40acc90320
sabah-hue <sabah.abdelbaset@gmail.com>
2023-11-29 03:26:02 +0200

    My First commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for letter in range(ord('a'), ord('z'), 1):
-        if c == ord(letter):
+        if ord(c) == ord(letter):
             return True
         else:
             return False
