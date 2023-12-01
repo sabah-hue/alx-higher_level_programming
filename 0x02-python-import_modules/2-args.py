b9a4c1b4036fee9f6f590bfc3f6dc2ceff843a7b
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-01 04:29:32 +0200

    My First commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,12 @@ if __name__ == "__main__":
 
     args = sys.argv
     args_len = len(args) - 1
+
     if args_len == 0:
         print("0 arguments.")
     elif args_len == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(args_len))
-    for i in range(1:args_len + 1):
-        print("{}: {}".format(i, args[i + 1]))
+    for i in range(args_len):
+        print("{}: {}".format(i + 1, args[i + 1]))
