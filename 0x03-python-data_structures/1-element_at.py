f4456636d6fea022f5a161f78bb60ea0f75299a1
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-03 04:09:47 +0200

    My First commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,7 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     x = len(my_list)
-    if idx > x or idx == x:
+    if idx >= x:
         return None
-    return my_list[idx]
+    else:
+        return my_list[idx]
