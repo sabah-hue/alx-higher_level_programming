2e72b75117cf154ec880c1e28290f81eb0649c35
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-03 04:05:42 +0200

    My First commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     x = len(my_list)
-    if idx > x:
+    if idx > x or idx == x:
         return None
     return my_list[idx]
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,3 @@
+#!/user/bin/python3
+def replace_in_list(my_list, idx, element):
+
