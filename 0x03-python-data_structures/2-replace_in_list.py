a898f5ef635ff17c28d396b4f4bb1dad1568ec24
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-03 04:17:28 +0200

    My First commit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,8 @@
 #!/user/bin/python3
 def replace_in_list(my_list, idx, element):
-
+    x = len(my_list)
+    if idx < 0 or idx >= x:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
