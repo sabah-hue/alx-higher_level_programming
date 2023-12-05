221fc4467cd8d6656666bfc787a9d646bd40d6c3
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-05 04:55:20 +0200

    My First commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new_str = ''
+    new_str = ""
     for i in my_string:
         if i != 'c' and i != 'C':
-            new_str = new_str + i
+            new_str += i
     return new_str
