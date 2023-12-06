feb79c54f9f8d12c3d37cd4b7a8fd98fdb4145a5
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-06 04:45:19 +0200

    My First commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            if c == len(matrix[r]) - 1:
+                print('{:d}'.format(matrix[r][c]), end='')
+            else:
+                print('{:d}'.format(matrix[r][c]), end=' ')
+        print()
