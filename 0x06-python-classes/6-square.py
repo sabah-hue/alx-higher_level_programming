13243ee26e1015fadb3a51588cda0c5a941e4f92
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-20 04:54:50 +0200

    My First commit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""mod def """
+
+
+class Square:
+    """ class square """
+
+     def __init__(self, size=0, position=(0, 0)):
+         self.size = size
+         self.position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+              raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+         return (self.__size ** 2)
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(self.position[1]):
+                print("")
+            for i in range(self.size):
+                for j in range(self.position[0]):
+                     print(" ", end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print("")
