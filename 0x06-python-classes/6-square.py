894f5af0526ab74664b5d9f39f938ab96a39ea50
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-20 04:56:58 +0200

    My First commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """ class square """
 
-     def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
          self.size = size
          self.position = position
 
