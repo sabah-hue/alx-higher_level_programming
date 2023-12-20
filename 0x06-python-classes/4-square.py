80be5a233c23369124e45932d65e9c636344def1
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-20 03:41:24 +0200

    My First commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" mod def """
+
+
+class Square:
+    """ class """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
