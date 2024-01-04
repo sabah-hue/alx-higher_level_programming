f473979842099f671902d7b53350a9a8118bcca3
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-04 21:36:03 +0200

    My First commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,7 +29,6 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
