c14696edad6e8ea163a438f26473a43b629801c0
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-08 21:58:37 +0200

    My First commit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-    if type(obj) is a_class:
+    """ check if the object is exactly an instance of this class """
+    if  type(obj) is a_class:
         return True
     return False
