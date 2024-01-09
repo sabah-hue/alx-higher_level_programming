0481c1df81579bb0cc57325e480cb4cb89879ec9
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-09 03:45:10 +0200

    My First commit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,9 +5,9 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Improve Geometry """
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-
