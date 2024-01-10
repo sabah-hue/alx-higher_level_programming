dd0e330769aa3e630637c0f79f57221bb8539907
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-10 04:04:17 +0200

    My First commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module ..."""
+import json
+
+
+def load_from_json_file(filename):
+    """Create object from a JSON file"""
+    with open(filename, "r") as f:
+        json.load(f)
