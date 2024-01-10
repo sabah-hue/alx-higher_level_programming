e3801aa542a08ca240f71266d37628d2c9bddf29
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-10 04:27:21 +0200

    My First commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ module ..."""
-from sys import argv
 
+
+from sys import argv
 
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
