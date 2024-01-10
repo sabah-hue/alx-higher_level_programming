62797764bf1a52ba7468078e982db5710f3886b2
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-10 03:48:33 +0200

    My First commit

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module ... """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
