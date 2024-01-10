6f40afbcdffd3d4926086460c8f5c8c25c8dc26e
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-10 03:19:48 +0200

    My First commit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module ... """
+
+
+def append_write(filename="", text=""):
+    """ appends a string at the end of a text file """
+    with open(filename, "a") as f:
+        return (f.wrie(text))
