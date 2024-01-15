cb6ed3a853d41e346d7ef37b792cc4d7567892d0
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-16 01:29:17 +0200

    My First commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,10 +10,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ constractor """
         super().__init__(size, size, x, y, id)
-        self.size = size
-        self.x = x
-        self.y = y
-        self.id = id
 
     @property
     def size(self):
