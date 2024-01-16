7955f8200b66ace44ca5e44f70c625caab95cc9f
sabah-hue <sabah.abdelbaset@gmail.com>
2024-01-16 02:27:32 +0200

    My First commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,13 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create dic """
+        if cls.__name__ == "Rectangle":
+            d = cls(1, 1)
+        if cls.__name__ == "Square":
+            d = cls(1)
+        d.update(**dictionary)
+        return d
