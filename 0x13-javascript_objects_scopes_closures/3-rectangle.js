2f6be33ef3129e708c54065cc4d9f71622626fd7
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-13 04:54:49 +0200

    print rect

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,5 +7,11 @@ class Rectangle {
 			this.height = h;
 		}
 	}
+    print () {
+        for (let i = 0; i < this.height; i++)
+        {
+            console.log('X'.repeat(this.width));
+        }
+    }
 }
 module.exports = Rectangle;
