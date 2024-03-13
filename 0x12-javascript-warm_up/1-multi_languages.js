4f4e2279cf3b80244fab77acdc4713e30b69c7ac
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-13 07:40:41 +0200

    style issue

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let x = 'C is fun';
-let y = 'Python is cool';
-let z = 'JavaScript is amazing';
-console.log(x);
-console.log(y);
-console.log(z);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
