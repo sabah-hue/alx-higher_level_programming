9c43f8d8d93d6d6ff774a2c3a3c980a6c7fd4d83
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-14 03:18:38 +0200

    square issue

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const args = process.argv;
 const number = Math.floor(Number(args[2]));
 if (args[2] && Number.isInteger(number)) {
   for (let i = 0; i < number; i++) {
-    console.log('x'.repeat(number));
+    console.log('X'.repeat(number));
   }
 } else {
   console.log('Missing size');
