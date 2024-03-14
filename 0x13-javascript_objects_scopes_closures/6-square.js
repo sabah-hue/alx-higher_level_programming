6dc8f2247e4e7bfb1adfd36c13cde7518815f7f3
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-15 01:53:55 +0200

    6-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ class Square extends square {
   charPrint (c) {
     if (c) {
       for (let i = 0; i < this.size; i++) {
-        console.log('C'.repeat(this.size));
+        console.log(c.repeat(this.size));
       }
     } else {
       this.print();
