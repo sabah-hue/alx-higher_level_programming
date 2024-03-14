46456e84d1d3388327e97f518455702601525fa2
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-14 03:11:16 +0200

    add number

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const number = Number(args[2]);
+if (args[2] && Number.isInteger(number)) {
+  for (let i = 0; i < number; i++) {
+    console.log('x'.repeat(number));
+  }
+} else {
+  console.log('Missing size');
+}
