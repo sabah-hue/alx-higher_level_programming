624308384e9ae03b5bef2718d038beea65972e84
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-14 03:16:13 +0200

    square issue

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv;
-const number = Number(args[2]);
+const number = Math.floor(Number(args[2]));
 if (args[2] && Number.isInteger(number)) {
   for (let i = 0; i < number; i++) {
     console.log('x'.repeat(number));
