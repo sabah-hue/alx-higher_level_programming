0dc3edf99ed391ebcff56d5c540e63492940d60f
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-16 07:33:27 +0200

    pystylecode issue

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     # use BINARY for uppercase sensetive
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states\
+                 WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
