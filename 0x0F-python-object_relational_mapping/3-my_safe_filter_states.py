1ce3a0a8896f0cbd66285a91f5575fe58b495e60
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-17 05:59:39 +0200

    safe data

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and
+displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """ run in CMD not in imported """
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT * FROM states\
+                 WHERE name LIKE BINARY %s ORDER BY id ASC"
+    cur.execute(query, (argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
