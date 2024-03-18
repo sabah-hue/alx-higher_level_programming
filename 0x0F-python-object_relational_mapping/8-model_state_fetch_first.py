6704e7aba89488f290b9929e75a65cc993b668a0
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-18 15:26:46 +0200

    output issue

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,4 +16,6 @@ if __name__ == "__main__":
     state = session.query(State).first()
     if state:
         print("{}: {}".format(state.id, state.name))
+    else:
+        print("")
     session.close()
