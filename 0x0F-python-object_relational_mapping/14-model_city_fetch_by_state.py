3a9d2ab250c19c00989b0710d5b447766ecf2779
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-20 07:40:44 +0200

    style issue

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-    session = Session() 
-    for city, state in session.query(City).join(State, City.state_id==State.id).all():
+    session = Session()
+    for city, state in session.query(City)
+    .join(State, City.state_id == State.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
