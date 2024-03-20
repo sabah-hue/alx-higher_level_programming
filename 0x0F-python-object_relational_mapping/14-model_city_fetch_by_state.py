8ff5ec3ee8ad02870bb547d0978ac2301925527a
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-20 05:26:06 +0200

    join

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that  prints all City objects from the database
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import State, Base
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session() 
+    for city, state in session.query(City).join(State, City.state_id==State.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module definition of a State """
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ City class """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(256), nullable=False)
+    state_id = Column(Integer, ForeignKey('states_id'), nullable=False)
