a8e0524221b7b9fa830aa354f509c82464622eeb
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-23 12:09:51 +0200

    documentation issue

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,7 +3,6 @@
 script that creates the State “California” with
 the City “San Francisco” from the database hbtn_0e_100_usa
 """
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
@@ -16,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-    session = Session() 
+    session = Session()
     session.add(State(name="California"))
     session.add(City(name="San Francisco", state=california))
     session.commit()
