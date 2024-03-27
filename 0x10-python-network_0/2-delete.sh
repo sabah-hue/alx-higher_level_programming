7b60171ab2efc2dbe432ee7efed2814744b805bb
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-27 23:11:12 +0200

    debug

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a DELETE request to the URL 
-curl -s DELETE $1 -L
+curl -sX DELETE $1 -L
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to the passed URL
-curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
+curl -sX POST $1 -L -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
