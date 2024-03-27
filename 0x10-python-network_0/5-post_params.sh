8dee5946036f361eafcfda420a7443a9575c6c42
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-27 23:05:24 +0200

    debug

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to the passed URL
-curl -s "$1" -L POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX "$1" -L POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
