5d577ba8d80a426127c1aeff4e0874e6d30ac3ae
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-29 04:48:36 +0200

    post req

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter """
+import urllib.request
+from sys import argv
+if __name__ == "__main__":
+    link = argv[1]
+    values = {'email':argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(link, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode("utf-8")
+        print(html)
