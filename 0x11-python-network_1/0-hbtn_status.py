645c6c0b0d8319667ff99d5531cf4a42dee605a9
sabah-hue <sabah.abdelbaset@gmail.com>
2024-03-29 03:42:17 +0200

    style issue

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,8 @@
 """ script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    link = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(link) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
