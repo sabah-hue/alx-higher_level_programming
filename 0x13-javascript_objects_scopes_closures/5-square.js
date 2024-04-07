47a7449cef1b94003c5bb13b720c981ccc1e099c
sabah-hue <79392460+sabah-hue@users.noreply.github.com>
2024-04-08 01:44:52 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const rectangle = require('./4-rectangle.js');
-class Square extends rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-}
-module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  const x = list.filter(e => e === searchElement);
-  return x.length;
-};
