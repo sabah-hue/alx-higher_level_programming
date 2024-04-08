548a4cd8a77e2fb93879e96142ee793c97390d30
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-08 15:10:47 +0200

    first commit

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args[2] === undefined) {
-  console.log('No argument');
-}
-for (let i = 2; i < args.length; i++) {
-  console.log(args[i]);
-}
+console.log(`${args[2]} is ${args[3]}`);
