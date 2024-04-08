f00f5ec469ffae006cb2edb7f24dd4d46f10bb72
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-08 14:33:47 +0200

    use args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const args = process.argv;
+if (args[2] === undefined) {
+  console.log('No argument');
+}
+if (args.length === 3) {
+  console.log('Argument found');
+}
+if (args.length > 3) {
+  console.log('Arguments found');
+}
