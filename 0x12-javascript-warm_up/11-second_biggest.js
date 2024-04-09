026a0adec6f50797af49c2c091698e1ea3697f1e
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-09 03:26:08 +0200

    try with sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
 const args = process.argv;
+const arrCopy = args.slice(2);
 if (args[2] === undefined || args[3] === undefined) {
-  console.log('0');
+  console.log(0);
 } else {
-  let max = args[2];
-  for (let i = 2; i < args.length; i++) {
-    if (args[i] > max) {
-      max = args[i];
-    }
-  }
-  console.log(max);
-  let secMax = 0;
-  for (let i = 2; i < args.length; i++) {
-    if (args[i] > secMax && args[i] !== max) {
-      secMax = args[i];
-    }
-  }
-  console.log(secMax);
+  arrCopy.sort((a, b) => {
+    return b - a;
+  });
+  console.log(arrCopy[1]);
+  // let max = args[2];
+  // for (let i = 2; i < args.length; i++) {
+  //   if (args[i] > max) {
+  //     max = args[i];
+  //   }
+  // }
+  // console.log(max);
+  // let secMax = 0;
+  // for (let i = 2; i < args.length; i++) {
+  //   if (args[i] > secMax && args[i] !== max) {
+  //     secMax = args[i];
+  //   }
+  // }
+  // console.log(secMax);
 }
