ccf212d9d5e06f1b230738ab40052892d2006394
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-09 02:45:25 +0200

    output issue

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
 function factorial (n) {
-  if (n === NaN) {
+  if (isNaN(n) || n === 1) {
     return 1;
   }
   return n * factorial(n - 1);
 }
-factorial(parseInt(args[2]));
+console.log(factorial(parseInt(args[2])));
