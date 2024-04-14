46c7c0c198bda56e7ed9a23ccab41fddc3e69318
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-14 20:01:10 +0200

    safe from sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
                            user=argv[1], passwd=argv[2],
                            db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE \
-    name LIKE BINARY '{:s}' ORDER BY id ASC".format(argv[4]))
+    query = "SELECT * FROM states WHERE \
+    name LIKE BINARY %s ORDER BY id ASC"
+    cur.execute(query, (argv[4], ))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
