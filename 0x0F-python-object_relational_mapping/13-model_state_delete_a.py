6561863e6a8ce00ddf5931b33f47f21e8b5e6595
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-15 05:00:55 +0200

    output issue

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.name.contains('a')).all():
+    for state in session.query(State).filter(State.name.like('%a%')).all():
         session.delete(state)
     session.commit
     session.close()
