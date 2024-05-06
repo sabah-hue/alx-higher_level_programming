cc45bd209b2077157d4f6b54c14af7e36358a95f
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-05 23:12:59 -0700

    print content length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a request to URL, and displays the size of the body of the response
-curl $1 | grep -b "content-Length" 
+curl -sI $1 | grep -i Content-Length | awk '{print $2}'
